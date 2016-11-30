8174aba1206f449a19d056709976622dd349419e
Chris Wieber <chris@mediawagon.net>
2016-11-29 21:35:21 -0600

    Resume view and test

--- tests/resume_tests/TestResumeView.php
+++ tests/resume_tests/TestResumeView.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ResumeViewTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testResumePageShows()
     {
-        $this->visit('/')
-             ->see('Oh hi.');
+        $this->visit('/resume')
+            ->see('Christopher Wieber');
     }
 }
