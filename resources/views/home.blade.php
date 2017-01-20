c5c52961540a9ef114804fa3d6d11b84e3d9430c
Chris Wieber <chriswieber@miad.edu>
2017-01-19 19:19:30 -0600

    Added some booga booga

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Oh, Hi.</h1>
-    <p>I'm Chris Wieber. I make stuff for the web, and occasionally venture beyond. Join me as I journey to become a better programmer,
+    <p>I'm Chris Wieber. I make stuff for the web, BOOGA BOOGA! and occasionally venture beyond. Join me as I journey to become a better programmer,
     runner, and all-around kinda guy.</p>
     <h1>What I've Been Up To Lately:</h1>
     <p>[Activity feed goes here. Yeah, I need to make an activity feed].</p>
